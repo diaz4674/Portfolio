f1214d6947bccf3c2a1d53a2611a976df18016aa
Miguel <diaz4674@gmail.com>
2019-06-19 22:53:58 -0700

    styled CSS animation updates/flexbox

---
+++ script.js
@@ -0,0 +1,8 @@
+//SkillsButton Section 
+$(".skillsButton").on( 'click', function() {
+  $(".hiddenSkills").css({display:'block', position: 'relative' }) 
+  $(".skillsButton").css({display:'none'})
+   $(skillsContainer).css({
+     height: 'auto',
+    })
+})
